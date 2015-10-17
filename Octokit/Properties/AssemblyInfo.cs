ec2fdbc7295864e39816190df5c8cd41991ba906
Haroon <haroon.ahmed25@gmail.com>
2015-10-17 22:38:25 +0100

    Clean up the pages branch

--- Octokit/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Octokit")]
-[assembly: AssemblyDescription("An async-based GitHub API client library for .NET")]
-[assembly: InternalsVisibleTo("Octokit.Tests")]
-[assembly: InternalsVisibleTo("Octokit.Tests-NetCore45")]
-[assembly: InternalsVisibleTo("Octokit.Tests-Portable")]
